348ac614f670fbd3b37650ad084e0b3dfebdaf95
Ulrich <nzeuleuu@gmail.com>
2024-08-01 11:43:29 +0100

    Partie 1 : Configuration du projet

---
+++ src/main/java/com/example/systemeGestionTickets/SystemeGestionTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemeGestionTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeGestionTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeGestionTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/systemeGestionTickets/SystemeGestionTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemeGestionTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeGestionTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
