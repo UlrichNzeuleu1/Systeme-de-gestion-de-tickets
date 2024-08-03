4a4ceaf8f8e356f0c0aca3c4a566746cb5c9680b
Ulrich <nzeuleuu@gmail.com>
2024-08-03 19:45:56 +0100

    commit security configuration

--- src/main/java/com/example/systemeGestionTickets/config/SecurityConfig.java
+++ src/main/java/com/example/systemeGestionTickets/config/SecurityConfig.java
@@ -1,44 +1,44 @@
-//package com.example.systemeGestionTickets.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@Configuration
-//@EnableWebSecurity
-//
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//            .csrf().disable()
-//            .authorizeRequests()
-//                .antMatchers("/api/tickets/**").hasRole("USER")
-//                .antMatchers("/api/users/**").hasRole("ADMIN")
-//                .anyRequest().authenticated()
-//            .and()
-//            .httpBasic();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication()
-//            .withUser("user")
-//            .password(passwordEncoder().encode("password"))
-//            .roles("USER")
-//            .and()
-//            .withUser("admin")
-//            .password(passwordEncoder().encode("password"))
-//            .roles("USER", "ADMIN");
-//    }
-//}
+package com.example.systemeGestionTickets.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("/api/tickets/**").hasRole("USER")
+                .antMatchers("/api/users/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+            .and()
+            .httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user")
+            .password(passwordEncoder().encode("password"))
+            .roles("USER")
+            .and()
+            .withUser("admin")
+            .password(passwordEncoder().encode("password"))
+            .roles("USER", "ADMIN");
+    }
+}
